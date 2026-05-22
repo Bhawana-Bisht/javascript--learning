bcb1f39b6bd68ae44370be3b8a5d7ce6588b9207
Bhawana Bisht <bhawanab2003@gmail.com>
2026-05-22 17:27:02 +0000

    Learned JavaScript variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountid=144553
+let accountEmail="bhawana_13@gamil.com"
+var accountpassword="12345" 
+let accountstate
+accountcity ="jaipur"
+//accountid=2 //
+accountEmail="bhawna@gmail.com"
+//console.log(accountid);
+//console.log(accountEmail);
+accountpassword =4567;
+//console.log(accountpassword);
+accountcity="haldwani"
+//console.log(accountcity)
+console.table([accountid, accountEmail,accountpassword, accountcity,accountstate])
+/*  pefer not use var in js because of  issue in block and  functional scope */
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer varsion
+ //alert(3+3) // we are using node js , not browser
+let name="bhawana"
+let age=21
+let isloggedin =false
+let state;
+//number => 2 to  power 53
+//bigint
+//string =>"
+// //boolean=> true/false
+ //null=>standalone value
+ //undefined
+ //symbol =>unique
+//object
+console.log(typeof 132)
+console.log(typeof "kavya")
+ console.log(typeof null)
+ console.log(typeof age)
+ console.log(typeof undefined)

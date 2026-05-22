8221a84c580e10c083bd5273dad713df4dea0e3e
Bhawana Bisht <bhawanab2003@gmail.com>
2026-05-22 18:13:52 +0000

    learn about type conversion in js  with different  example

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,21 @@
+let age= 33
+console.log(typeof age)
+let score=45
+
+console.log(typeof score)
+let valueInNumber= Number(score)
+console.log(valueInNumber);
+// "33" =>33\
+//"33rbgc"=> NaN( Not a number)
+//true=>1; false=>0;
+ //let IsloggedIn= true
+let IsloggedIn=""
+let booleanIsloggedIn= Boolean(IsloggedIn)
+console.log(booleanIsloggedIn );
+// 1=>true ; 0=>false
+//"" => false
+//" bhawana"=> true
+let someNumber= 78
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof  stringNumber)

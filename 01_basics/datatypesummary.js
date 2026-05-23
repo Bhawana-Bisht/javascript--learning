a78b03c2880c00ffb072e993b6bd98abdba5b22a
Bhawana Bisht <bhawanab2003@gmail.com>
2026-05-23 06:40:23 +0000

    datype summary with example

---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,22 @@
+//datatypes:
+   //1- primitive  => 7 types 
+      //[ string, number, boolean, null , undefined , symbol , bigInt]
+
+         // 2- non-primitive( reference datatypes)
+             //> array
+                   // example - 
+const  heros =[" superman", " saktiman"]
+
+  //> object
+ // example -  
+  let myObj ={
+name:" bhawana",
+ age :13,
+       }
+  //> function
+// defined as  function(){} 
+ // example
+  const myfunction = function(){
+console.log("hello world")  }
+
+                                                                                                         

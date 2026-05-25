b393c44b7a0694ffdb74a6e71c0c64583720b3b6
Bhawana Bisht <bhawanab2003@gmail.com>
2026-05-25 08:31:40 +0000

    nums and maths

---
+++ 01_basics/05_numsandmaths.js
@@ -0,0 +1,38 @@
+  // numbers
+  const balance = new Number(200)
+ console.log(balance)
+ 
+ console.log(balance.toString())
+ 
+ console.log(balance.toFixed(2))
+ console.log(balance.toPrecision(3))
+ const othernumber= 123.8966
+ console.log(othernumber.toFixed(2))
+ console.log(othernumber.toPrecision(3))
+ const  hundred= 100000
+  console.log( hundred.toLocaleString())
+  console.log( hundred.toLocaleString('en-IN'))
+
+  // maths
+  console.log(Math);
+  console.log(Math.abs(-4));// abolute value neg  to pos
+  console.log(Math.round(2.34))
+  console.log(Math.min(9,8,7,6));
+  console.log(Math.max(98,0,87,6))
+  console.log(Math.random())
+  console.log(Math.random()*10)
+  console.log((Math.random()*10) +1) // incase value is 0
+ const min=10
+ const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+ min)
+
+
+  
+  
+  
+ 
+ 
+ 
+ 
+ 

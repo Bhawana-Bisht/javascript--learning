c035581fa25e19511c78da97ded6c6a1506f883b
Bhawana Bisht <bhawanab2003@gmail.com>
2026-05-25 07:19:14 +0000

    learn different method on string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,24 @@
+ const name= "bhawna"
+const repocount= 45
+   //console.log(name+ repocount+ "value")
+    console.log( `hello my name is ${ name}  and my git repo count is ${repocount}`);
+     const gamename= new String('bhawana_com_bisht')
+     //key value pair acess
+     console.log(gamename[1])
+     console.log(gamename.__proto__)
+     console.log(gamename.length)
+      console.log(gamename.toUpperCase())
+      console.log(gamename.charAt(2))
+      console.log(gamename.indexOf('w'))
+       const  newString = gamename.substring(0,4)
+       console.log(newString);
+        const  anotherString = gamename.slice(-4,4)
+       console.log(anotherString)
+       const  anothernewString = "  bhawana  "
+      console.log(anothernewString);
+       console.log(anothernewString.trim());
+        const url="https://bhawana.com/bhawana%20bisht"
+         console.log(url.replace('%20','_'))
+       console.log(url.includes('bisht'))
+       console.log(gamename.split('_'));
+       

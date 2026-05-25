6b842fa333289eb52d6c181ee2d593ae2007510c
Bhawana Bisht <bhawanab2003@gmail.com>
2026-05-25 09:15:17 +0000

    date and time different methods

---
+++ 01_basics/06_dateandtime.js
@@ -0,0 +1,26 @@
+//dates
+ let myDate= new Date()
+ console.log(myDate.toDateString())
+ console.log(myDate.toLocaleString());
+ console.log(myDate.toString());
+ console.log(myDate.toTimeString());
+console.log(typeof(myDate));
+
+ let  mycreatedate= new  Date(2026,4,23)
+ console.log(mycreatedate.toDateString());
+ console.log(mycreatedate.toLocaleString());
+ let  createdate= new  Date("01-14-2023")
+ console.log(createdate.toLocaleString());
+
+ let timestamp =Date.now()
+ console.log(timestamp)
+ console.log(timestamp);
+ console.log(mycreatedate. getTime());
+ console.log(mycreatedate. getHours());
+ console.log(mycreatedate. getMinutes());
+ console.log(mycreatedate. getSeconds());
+ console.log(mycreatedate. getDay());
+  
+
+ 
+ 

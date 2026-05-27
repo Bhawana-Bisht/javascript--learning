bd3136ffdb7599b3db179e45d1f4a0b6b0b0574f
Bhawana Bisht <bhawanab2003@gmail.com>
2026-05-27 06:47:40 +0000

    Different  methods of array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+const myArr =[0,1,2,3,4,5]
+//console.log( myArr[1]);
+// array method
+ //myArr.push(6,7,8)
+//console.log(myArr);
+// myArr.pop()
+ //console.log(myArr);
+ //unshift =>add vaule at index 0 
+ // myArr.unshift(3)
+  //console.log(myArr);
+  //shift=> remove unshift value 
+ //myArr.shift()
+ // console.log(myArr);
+//console.log(myArr.includes(7));
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(3))
+//const newarr= myArr.join()
+//console.log(myArr);
+//console.log(newarr);
+
+//slice and splice difference
+ console.log("A", myArr);
+ const myn1= myArr.slice(1,3)
+ console.log(myn1);
+ console.log("B", myArr);
+ const myn2= myArr.splice(1,3)
+ console.log(myn2);
+ console.log("B", myArr);
+
+ 
+ 
+
+  
+ 
+
